bf5feef85e9725a9c90b4be0ee4ddf1522fb805b
Blane Nelson <blane.nelson@tuppence.io>
2018-10-19 09:42:36 -0600

    Copy the data from source database/tables to the target database/tables.

--- Stratis.Utility.CompressDatabase/CommandCompressExternal.cs
+++ Stratis.Utility.CompressDatabase/CommandCompressExternal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using DBreeze;
 
 namespace Stratis.Utility.CompressDatabase
 {
@@ -7,12 +8,17 @@ namespace Stratis.Utility.CompressDatabase
     {
         public int Execute(string dataDir, string tempDir, bool deleteTempDir)
         {
+            Console.WriteLine($"Compressing database external, data directory = {dataDir}, temp directory = {tempDir}");
+            
             // Create the temp directory if needed.
             var ret = ValidateTempDirectory(tempDir);
             if (ret != 0)
             {
                 return ret;
             }
+
+            CopyDatabaseData(dataDir, tempDir);
+            CopyDatabaseFiles(dataDir, tempDir);
 
             Console.WriteLine($"Successfully completed compressing the database.");
             return 0;
@@ -47,6 +53,61 @@ namespace Stratis.Utility.CompressDatabase
             }
 
             return 0;
+        }
+
+        private void CopyDatabaseData(string dataDir, string tempDir)
+        {
+            foreach (var repository in Consts.Repositories)
+            {
+                var sourceRrepoFolder = Path.Combine(dataDir, repository);
+                var targetRrepoFolder = Path.Combine(tempDir, repository);
+
+                Console.WriteLine($"Copying repository {repository}");
+
+                using (var sourceDBreeze = new DBreezeEngine(sourceRrepoFolder))
+                using (var targetDBreeze = new DBreezeEngine(targetRrepoFolder))
+                {
+                    CopyTablesData(sourceDBreeze, targetDBreeze, repository);
+                }
+            }
+        }
+
+        private void CopyTablesData(DBreezeEngine sourceDBreeze, DBreezeEngine targetDBreeze, string repository)
+        {
+            var tables = sourceDBreeze.Scheme.GetUserTableNamesStartingWith(string.Empty);
+            Console.WriteLine($"Found {tables.Count} table in {repository} database.");
+
+            foreach (var table in tables)
+            {
+                using (var soruceTrans = sourceDBreeze.GetTransaction())
+                using (var targetTrans = targetDBreeze.GetTransaction())
+                {
+                    var count = 0;
+                    var total = soruceTrans.Count(table);
+                    Console.WriteLine($"[{DateTime.Now}] Compressing {table} table in {repository} database with {total:n0} records.");
+
+                    foreach (var row in soruceTrans.SelectForward<byte[], byte[]>(table))
+                    {
+                        // Add record to the table in the target database
+                        targetTrans.Insert(table, row.Key, row.Value);
+
+                        count++;
+                        if (count % 10000 == 0)
+                        {
+                            targetTrans.Commit();
+                            Console.WriteLine($"[{DateTime.Now}] Processing {table} table, Copied {count:n0} of {total:n0} records.");
+                        }
+                    }
+
+                    // Finished copying of the records
+                    targetTrans.Commit();
+                    Console.WriteLine($"[{DateTime.Now}] Finished copying {table} table, Copied {count:n0} records.");
+                }
+            }
+        }
+
+        private void CopyDatabaseFiles(string dataDir, string tempDir)
+        {
         }
     }
 }
